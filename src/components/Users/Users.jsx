96afdfe320a7a3af5ecca280317eb6292bc8f01c
Direl21 <diana.tarabenko@gmail.com>
2021-09-15 19:00:27 +0200

    lol

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,14 +7,29 @@ class Users extends React.Component {
 
     componentDidMount() {
         axios
-            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
 
     render(){
+
+        let pagesCount = Math.ceil (this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+        for (let i=1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
         return <div>
+
+        <div>
+            {pages.map(p => {
+                return <span className={this.props.currentPage === p && classes.selectPage}>{p}</span> 
+            })}
+        </div>
+
         {
             this.props.users.map (u => <div className={classes.pad} key={u.id}>
                 <span>
